e193b4f59594582d89e377f2a945a6682b828df5
Dan Lohman <daniel.lohman@gmail.com>
2024-12-13 22:05:48 -0600

    Adding env vars etc

---
+++ .next/server/edge/chunks/ssr/edge-wrapper_a96a38.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "chunks/ssr/edge-wrapper_a96a38.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "chunks/ssr/edge-wrapper_a96a38.js",
+  "chunks": [
+    "chunks/ssr/_0c6da2._.js",
+    "chunks/ssr/src_middleware_ts_9f486c._.js"
+  ],
+  "source": "entry"
+});
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GnTE6M766Zgo5dEVaEXCQuMdoYdFEoXkdyDeiZCmwyg=\"\n}"
